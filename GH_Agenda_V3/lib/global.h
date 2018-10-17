92c66d22163041393cf88d636f570e2a7cdb36b8
Gilles Henrard <gilleshenrard@hotmail.com>
2018-10-17 15:53:58 +0200

    implement base comparison and swap functions

--- GH_Agenda_V3/lib/global.h
+++ GH_Agenda_V3/lib/global.h
@@ -3,14 +3,23 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct{
+typedef struct t_tuple t_tuple;
+
+struct t_tuple{
     int id;
     char lastname[28];
     char firstname[32];
     char city[32];
-    char filler[32];
-} t_tuple;
+    char filler[28];
+    t_tuple *next;
+};
+
+typedef enum {ID, LASTNAME, FIRSTNAME, CITY} e_critera;
 
 int newTuple(t_tuple*);
+int compareLastName(void*, void*);
+int compareFilterLastName(void*, void*);
+int compareID(void*, void*);
+int swapTuples(void*, void*);
 
 #endif // GLOBAL_H_INCLUDED
--- GH_Agenda_V3/src/global.c
+++ GH_Agenda_V3/src/global.c
@@ -18,3 +18,40 @@ int newTuple(t_tuple *newtuple){
 
     return 0;
 }
+
+int compareLastName(void* first, void* second){
+    t_tuple *first_tuple = (t_tuple*) first;
+    t_tuple *second_tuple = (t_tuple*) second;
+
+    return stricmp(first_tuple->lastname, second_tuple->lastname);
+}
+
+int compareFilterLastName(void* first, void* second){
+    t_tuple *first_tuple = (t_tuple*) first;
+    t_tuple *second_tuple = (t_tuple*) second;
+    char tmp[28] = "0";
+
+    memcpy(tmp, first_tuple->lastname, strlen(second_tuple->lastname));
+    tmp[strlen(second_tuple->lastname)]='\0';
+
+    return stricmp(tmp, second_tuple->lastname);
+}
+
+int compareID(void* first, void* second){
+    t_tuple *first_tuple = (t_tuple*) first;
+    t_tuple *second_tuple = (t_tuple*) second;
+
+    return first_tuple->id - second_tuple->id;
+}
+
+int swapTuples(void* first, void* second){
+    t_tuple *first_tuple = (t_tuple*)first;
+    t_tuple *second_tuple = (t_tuple*)second;
+    t_tuple tmp;
+
+    tmp = *first_tuple;
+    *first_tuple = *second_tuple;
+    *second_tuple = tmp;
+
+    return 0;
+}
