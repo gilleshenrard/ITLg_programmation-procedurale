d3de7846088053891eb94549b2d95aa9650b0e84
Gilles Henrard <gilleshenrard@hotmail.com>
2018-10-17 15:54:20 +0200

    implement second menu choice

--- GH_Agenda_V3/main.c
+++ GH_Agenda_V3/main.c
@@ -3,14 +3,18 @@
 #include <stdio.h>
 #include "lib/global.h"
 #include "lib/inout.h"
+#include "lib/structure.h"
+#include "lib/algo.h"
 
 char filename[] = "test.dat";
 
 char menu(int, char[][32]);
+int searchList(FILE*, e_critera, int);
 
 int main(int argc, char *argv[])
 {
     FILE *file = NULL;
+    int filesize = 0;
     char choice=0;
     t_tuple current;
     char princ_menu[4][32]={"Creer des enregistrements",
@@ -26,11 +30,19 @@ int main(int argc, char *argv[])
                 newTuple(&current);
                 if((file = fopen(filename, "r+b")) == NULL)
                     file = fopen(filename, "w+b");
-                appendTuple(file, &current);
+                appendFile(file, &current);
                 fclose(file);
                 break;
 
             case '1':
+                file = fopen(filename, "rb");
+                if(file){
+                    fseek(file, 0, SEEK_END);
+                    filesize = ftell(file)/sizeof(t_tuple);
+                    fseek(file, 0, SEEK_SET);
+                    searchList(file, LASTNAME, filesize);
+                    fclose(file);
+                }
                 break;
 
             case '2':
@@ -64,4 +76,63 @@ char menu(int i, char sections[i][32]){
     choice = getch();
 
     return choice;
+}
+
+int searchList(FILE* file, e_critera critera, int filesize){
+    t_tuple* first=NULL;
+    t_tuple* last=NULL;
+    t_tuple tab[filesize];
+    t_tuple tmp;
+    t_tuple *cur=NULL, *prev=NULL;
+    char name[28]="0";
+    int search = 0;
+
+    for(int i=0 ; i<filesize ; i++)
+        if(fread(&tab[i], sizeof(t_tuple), 1, file) < 1)
+            return -1;
+
+    switch(critera){
+        case LASTNAME:
+            bubbleSort((void*)tab, filesize, sizeof(t_tuple), &compareLastName, &swapTuples);
+            break;
+
+        case ID:
+            bubbleSort((void*)tab, filesize, sizeof(t_tuple), &compareID, &swapTuples);
+            break;
+
+        default:
+            break;
+    }
+
+    printf("\n-------------------------------------");
+    printf("\nSaisissez le nom de famille a chercher : ");
+    fflush(stdin);
+    scanf("%s", name);
+
+    strcpy(tmp.lastname, name);
+    search = binarySearch((void*)tab, filesize, sizeof(t_tuple), &compareFilterLastName, (void*)&tmp);
+    if(search < 0)
+        printf("\nnon-trouve");
+    else{
+        while(compareFilterLastName((void*)&tab[search], (void*)&tmp) <= 0 && search < filesize){
+            appendList(&first, &last, &tab[search]);
+            search++;
+        }
+        cur = first;
+        do{
+            printf("\n%s", cur->lastname);
+            cur = cur->next;
+        }while(cur);
+
+        cur = first;
+        do{
+            prev = cur;
+            cur = cur->next;
+            free(prev);
+        }while(cur);
+    }
+    fflush(stdin);
+    getch();
+
+    return 0;
 }
