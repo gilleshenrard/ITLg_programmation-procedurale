72f982a0ed931045a643a80bccdf43833a005dbc
Gilles Henrard <gilles.henrard@hotmail.com>
2019-01-27 15:17:48 +0100

    create base film structure

--- lib/film.h
+++ lib/film.h
@@ -1,6 +1,11 @@
 #ifndef FILM_H_INCLUDED
 #define FILM_H_INCLUDED
 
-
+typedef struct Film
+{
+    uint nb_scene;      // nombre de scènes dans le film
+    char nm_film[28];   // nom du film
+    int scene[32];      // taille en nombre d'images des 32 scènes
+} film;
 
 #endif // FILM_H_INCLUDED
