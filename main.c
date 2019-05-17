b56a7c184eb9a0b8a140e7b63b47eb00cdfc31f8
Gilles Henrard <gilles.henrard@hotmail.com>
2019-05-17 21:46:33 +0200

    import all libraries to final project

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "lib/algo.h"
 #include "lib/DB_Main.h"
 #include "lib/DB_Country.h"
+#include "lib/algo.h"
+#include "lib/DB_File.h"
+#include "lib/DB_Group.h"
+#include "lib/DB_Job.h"
+#include "lib/DB_Industry.h"
+#include "lib/DB_campaign.h"
+#include "lib/DB_Contact.h"
+
 
 int main(int argc, char *argv[])
 {
