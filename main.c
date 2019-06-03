bd31defce3984d3ad77faf58f3d1eeab72f16d37
Gilles Henrard <gilles.henrard@hotmail.com>
2019-06-04 00:13:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -139,9 +139,9 @@ void create_db(dbc* db){
     t_datablock index_block_cpy_grp = {&db->hdr.off_i_cpy_grp, &db->hdr.i_cpy_grp, sizeof(i_ccpy_grp)};
     t_datablock table_block_cpy = {&db->hdr.off_cpy, 0, sizeof(ccpy)};
     //contacts metadata
-//    t_algo_meta index_con = {NULL, db->nr_con, sizeof(i_ccon_cpy), compare_contact_cpy_index, swap_contact_index, assign_contact_index_cpy, assign_contact_index_slot, NULL, NULL, NULL, NULL};
-//    t_datablock index_block_con = {&db->hdr.off_i_con_cpy, &db->hdr.i_con_cpy, sizeof(i_ccon_cpy)};
-//    t_datablock table_block_con = {&db->hdr.off_con, 0, sizeof(ccon)};
+    t_algo_meta index_con = {NULL, db->nr_con, sizeof(i_ccon_cpy), compare_contact_cpy_index, swap_contact_index, assign_contact_index_cpy, assign_contact_index_slot, NULL, NULL, NULL, NULL};
+    t_datablock index_block_con = {&db->hdr.off_i_con_cpy, &db->hdr.i_con_cpy, sizeof(i_ccon_cpy)};
+    t_datablock table_block_con = {&db->hdr.off_con, 0, sizeof(ccon)};
     //campaign metadata
     t_algo_meta index_cam = {NULL, db->nr_cam, sizeof(i_ccam_PK), compare_campaign_PK_index, swap_campaign_index, assign_campaign_index_PK, assign_campaign_index_slot, NULL, NULL, NULL, NULL};
     t_datablock index_block_cam = {&db->hdr.off_i_cam_pk, &db->hdr.i_cam_pk, sizeof(i_ccam_PK)};
@@ -167,7 +167,7 @@ void create_db(dbc* db){
 
     Create_DB(db, DB_name);
     Import_CSV_company(db);
-//    Import_CSV_contact(db);
+    Import_CSV_contact(db);
     Import_CSV_campaign(db);
     Import_CSV_Country(db);
     Import_CSV_Group(db);
@@ -178,7 +178,7 @@ void create_db(dbc* db){
     create_index_file(db, &index_cpy, db->nr_cpy, &index_block_cpy, &table_block_cpy);
     printf("done\n");
     printf("Creating the index for Contacts (by company FK) : ");
-//    create_index_file(db, &index_con, db->nr_con, &index_block_con, &table_block_con);
+    create_index_file(db, &index_con, db->nr_con, &index_block_con, &table_block_con);
     printf("done\n");
     printf("Creating the index for Campaigns (by ID) : ");
     create_index_file(db, &index_cam, db->nr_cam, &index_block_cam, &table_block_cam);
